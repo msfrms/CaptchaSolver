dd8da18a91c38a11ea76fc33c4aa1c213a519f0d
Radaev Mikhail <msfrms@gmail.com>
2021-07-08 11:06:25 +0300

    add captcha image data to buffer

--- routes/api/captcha.js
+++ routes/api/captcha.js
@@ -3,6 +3,21 @@ var router = require('express').Router();
 const wrap = fn => (...args) => fn(...args).catch(args[2])
 
 router.get('/', wrap(async function (req, res) {
+    const response = await fetch('https://api.vk.com/captcha.php?sid=679196306792&s=1')
+    const imageData = await response.buffer()
+
+    const width = 128
+    const height = 64
+    const channels = 4
+
+    const resizeImageData = await sharp(imageData)
+        .resize(128, 64)
+        .ensureAlpha()
+        .raw()
+        .toBuffer();
+
+    const imageInputData = Float32Array.from(resizeImageData);
+
     res.send("need implemented")
 }));
 
