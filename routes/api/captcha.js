16029080a7e4bdde487a7c2edc72e52a32aa228d
Radaev Mikhail <msfrms@gmail.com>
2021-07-08 11:07:26 +0300

    add load pre trained model

--- routes/api/captcha.js
+++ routes/api/captcha.js
@@ -2,6 +2,11 @@ var router = require('express').Router();
 
 const wrap = fn => (...args) => fn(...args).catch(args[2])
 
+const captchaSession = new onnx.InferenceSession({ backendHint: "cpu" });
+const ctcSession = new onnx.InferenceSession({ backendHint: "cpu" });
+captchaSession.loadModel("models/captcha_model.onnx").then(_ => console.log("captcha_model loaded"));
+ctcSession.loadModel("models/ctc_model.onnx").then(_ => console.log("ctc_model loaded"));
+
 router.get('/', wrap(async function (req, res) {
     const response = await fetch('https://api.vk.com/captcha.php?sid=679196306792&s=1')
     const imageData = await response.buffer()
